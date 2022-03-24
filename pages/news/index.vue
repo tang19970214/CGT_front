37be97951958ef0b57c88fa121fe5c5ecd0e2983
George <tang19970214@gmail.com>
2022-03-24 11:34:41 +0800

    none

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -35,7 +35,6 @@ export default {
     const res = await $api.news.load(listQuery);
     const { code, data } = res.data;
     if (code !== 200) return;
-    console.log("news", listQuery, data);
 
     return { imgUrl, listQuery, list: data };
   },
